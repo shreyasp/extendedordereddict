0984535a3e76c4a01993bdd18593b2275e692990
Shreyas Patil <shreyasp@users.noreply.github.com>
2017-08-16 01:19:37 +0530

    [Major] Completed ExtendedOrderedDict, need to complete unit-test cases

--- extendedordereddict/extended_ordered_dict.py
+++ extendedordereddict/extended_ordered_dict.py
@@ -7,35 +7,111 @@ class ExtendedOrderedDict(OrderedDict):
 
     # Override to instantiate Order Indices list
     def __new__(cls, *args, **kwargs):
-        _instance = OrderedDict.__new__(cls, *args, **kwargs)
-        _instance.__order_index = list()
-        return _instance
+        __instance = OrderedDict.__new__(cls, *args, **kwargs)
+        __instance.__items = list()
+        __instance.__uniq_items = set()
+        return __instance
 
     # Overriding __setitem__, to append each new key to the Order Indices list
     def __setitem__(self, key, value, dict_setitem=dict.__setitem__):
         super(ExtendedOrderedDict, self).__setitem__(key, value)
-        self.__order_index.append(key)
-        # TODO: Remove after final commit
-        # print(self.__order_index)
+
+        if key not in self.__uniq_items:
+            self.__uniq_items.add(key)
+            self.__items.append(key)
+
         return self
 
-    def print(self):
-        pass
+    def __delitem__(self, key, dict_delitem=dict.__delitem__):
+        super(ExtendedOrderedDict, self).__delitem__(key)
+        self.__items.remove(key)
+        return self
+
+    def __repr__(self):
+        if not len(self.items()):
+            return '%s()' % (self.__class__.__name__,)
+
+        return '%s(%r)' % (self.__class__.__name__, self.items())
+
+    def __update_items(self, index, *args, **kwargs):
+        if not args and not kwargs:
+            raise Exception('Needs object to inserted at given position')
+
+        keys = list()
+        to_update = dict()
+        if args:
+            obj = args[0]
+            keys.extend(obj.keys())
+            to_update.update(obj)
+
+        if kwargs:
+            to_update = kwargs
+            keys = list(to_update.keys())  # Returns dict_keys
+
+        if(len(keys) > 1):
+            raise Exception('Cannot insert multiple keys at same index')
+
+        self.update(to_update)
+
+        # If key is not already present add to set and update items
+        # otherwise update the key position to new index
+        if keys[0] not in self.__uniq_items:
+            self.__uniq_items.add(keys[0])
+            self.__items.insert(index, keys[0])
+
+        else:
+            self.__items.remove(keys[0])
+            self.__items.insert(index, keys[0])
+
+    def iteritems(self):
+        for key in self.__items:
+            yield (key, self.get(key))
+
+    def popitem(self, last=True):
+        if last:
+            key = self.__items[-1]
+
+        else:
+            key = self.__items[0]
+
+        value = self.pop(key)
+        return key, value
+
+    def items(self):
+        return [(item, self.get(item)) for item in self.__items]
+
+    def keys(self):
+        return self.__items
+
+    def values(self):
+        return [self.get(key) for key in self.__items]
 
     def print_at_index(self, index):
-        pass
+        try:
+            _item = self.__items[index]
+            print(self.get(_item))
 
-    def insert_at_index(self, index):
-        pass
+        except IndexError as error:
+            raise(error)
 
-    def insert_before_index(self, index):
-        pass
+    def get_at_index(self, index):
+        print(self.__items)
 
-    def insert_after_index(self, index):
-        pass
+        try:
+            key = self.__items[index]
 
+        except IndexError as error:
+            raise(error)
 
-if __name__ == '__main__':
-    # e1 = ExtendedOrderedDict({'a': 'apple', 'b': 'ball'})
-    # e2 = ExtendedOrderedDict(a='apple', b='ball', c='cat')
-    e3 = ExtendedOrderedDict.fromkeys('a', 'apple')
+        return self.get(key)
+
+    def insert_at_index(self, index, *args, **kwargs):
+        self.__update_items(index, *args, **kwargs)
+
+    def insert_before_index(self, index, *args, **kwargs):
+        actual_index = index - 1
+        self.__update_items(actual_index, *args, **kwargs)
+
+    def insert_after_index(self, index, *args, **kwargs):
+        actual_index = index + 1
+        self.__update_items(actual_index, *args, **kwargs)
--- extendedordereddict/tests/test_extended_ordered_dict.py
+++ extendedordereddict/tests/test_extended_ordered_dict.py
@@ -1,5 +1,44 @@
 from unittest import TestCase
 from extendedordereddict.extended_ordered_dict import ExtendedOrderedDict
 
+
 class TestExtendedOrderedDict(TestCase):
-	pass
+
+    def test_extended_ordered_dict_type(self):
+        ex_od = ExtendedOrderedDict()
+        self.assertEqual(type(ex_od), ExtendedOrderedDict)
+
+    def test_extended_ordered_dict_items(self):
+        ex_od = ExtendedOrderedDict(a='Apple', g='Google', m='Microsoft')
+        tester_dict = dict(a='Apple', g='Google', m='Microsoft')
+
+        # TODO Need to implement _mapping for utilizing ItemsView() for
+        # ExtendedOrderedDict
+        tester_items = list(tester_dict.items())
+        ex_od_items = ex_od.items()
+
+        self.assertEqual(tester_items, ex_od_items)
+
+    def test_extended_ordered_dict_keys(self):
+        ex_od = ExtendedOrderedDict(a='Apple', g='Google', m='Microsoft')
+        tester_dict = dict(a='Apple', g='Google', m='Microsoft')
+
+        # TODO Need to implement _mapping for utilizing KeysView() for
+        # ExtendedOrderedDict
+        tester_items = list(tester_dict.keys())
+        ex_od_items = ex_od.keys()
+
+        self.assertEqual(tester_items, ex_od_items)
+
+    def test_extended_ordered_dict_values(self):
+        ex_od = ExtendedOrderedDict(a='Apple', g='Google', m='Microsoft')
+        tester_dict = dict(a='Apple', g='Google', m='Microsoft')
+
+        # TODO Need to implement _mapping for utilizing ValuesView() for
+        # ExtendedOrderedDict
+        tester_items = list(tester_dict.values())
+        ex_od_items = ex_od.values()
+
+        self.assertEqual(tester_items, ex_od_items)
+
+    # TODO: Test cases for insertion at/before/after index to be written
